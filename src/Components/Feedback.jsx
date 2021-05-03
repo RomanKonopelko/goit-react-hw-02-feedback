0857f2b75c7e264c4dd3b842e8c5d733edf8615e
RomanKonopelko <romank761@gmail.com>
2021-05-03 17:34:52 +0300

    refactor: change state to hooks

--- src/Components/Feedback.jsx
+++ src/Components/Feedback.jsx
@@ -1,46 +1,51 @@
-import { Component } from 'react'
+import { useState } from 'react'
 import Statistics from './Statistics'
 import Section from './Section'
 import FeedbackOption from './FeedbackOptions'
 
-export default class Feedback extends Component {
+export default function Feedback() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
-  }
-
-  addFeedback = (e) => {
+  const addFeedback = (e) => {
     const { name } = e.target
-
-    this.setState({ [name]: this.state[name] + 1 })
+    switch (name) {
+      case 'good': setGood(good + 1)
+        break;
+      case 'neutral': setNeutral(neutral + 1)
+        break;
+      case 'bad': setBad(bad + 1)
+        break;
+      default:
+        return
+    }
   }
 
-  countTotalFeedback = () => {
+  const countTotalFeedback = () => {
 
-    return Object.values(this.state).reduce((acc, val) => {
+    return Object.values({ good, neutral, bad }).reduce((acc, val) => {
       return acc + val;
     }, 0)
   }
 
-  countPositiveFeedbackPercentage = () => {
-    const total = this.countTotalFeedback()
-    const percentage = Math.round(this.state.good / total * 100)
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback()
+    const percentage = Math.round(good / total * 100)
     return isNaN(percentage) ? 0 : percentage;
   }
 
-  render() {
-    const options = ['Good', 'Neutral', 'Bad']
-    const { good, neutral, bad } = this.state;
-    const { countTotalFeedback, countPositiveFeedbackPercentage, addFeedback } = this
-    return (<div>
+  const options = ['Good', 'Neutral', 'Bad']
+
+  return (
+
+    <div>
       <Section title="Leave your feedback"><FeedbackOption options={options} onLeaveFeedback={addFeedback} /></Section >
-      <Section title='Statistics'>{this.countTotalFeedback() === 0 ? 'There is no feedback yet' : <Statistics good={good}
+      <Section title='Statistics'>{countTotalFeedback() === 0 ? 'There is no feedback yet' : <Statistics good={good}
         neutral={neutral}
         bad={bad}
         total={countTotalFeedback}
         positivePercentage={countPositiveFeedbackPercentage} />}</Section >
-    </div>)
-  }
+    </div>
+  )
 }
